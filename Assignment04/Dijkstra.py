8702b77d740b25b8de55578dba84eba68d5504fb
kris <retreat896@gmail.com>
2025-05-06 15:28:55 -0500

    added basic file structure for other files

---
+++ Assignment04/Dijkstra.py
@@ -0,0 +1,43 @@
+import csv
+import os
+from collections import defaultdict, deque
+import sys
+
+def build_adjacency_list(n, edges):
+    adj_list = defaultdict(list)
+    for u, v in edges:
+        adj_list[u].append(v)
+        adj_list[v].append(u)
+    return adj_list
+
+def dijkstra(n, adj_list, start):
+    visited = [False] * n
+    result = []
+    queue = deque([start])
+    visited[start] = True
+    
+    while queue:
+        return
+    return results
+
+def read_graph_from_csv(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        n = int(lines[0])  
+        edges = []
+
+        for line in lines[1:-1]:
+            if ',' in line:
+                u, v = map(int, line.strip().split(','))
+                edges.append((u, v))
+        start_vertex = int(lines[-1])  
+        return n, start_vertex, edges
+
+if __name__ == "__main__":
+    file_path = sys.argv[1] 
+    n, start_vertex, edges = read_graph_from_csv(file_path)
+    
+    adj_list = build_adjacency_list(n, edges)
+    result = dijkstra(n, adj_list, start_vertex)
+    
+    print(f"{result}")
